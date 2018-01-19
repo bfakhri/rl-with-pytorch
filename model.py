a9c503f2b3917a2d120a2c4abf4f846e99c2cda9
tubi <bfakhri@asu.edu>
2018-01-19 09:47:40 -0700

    Working playable environment

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-class model():
+class Model():
     def __init__(self, act_size):
         self.act_size = act_size
 
@@ -10,14 +10,11 @@ class model():
         action = np.zeros(self.act_size, dtype=np.int)
         act_idx = np.random.randint(0, len(action))
         action[act_idx] = 1
-        return action
+        #return action
+        return act_idx
 
     def learn(self, replay_buffer):
         "Performs backprop w.r.t. the replay buffer"
         return 0
 
-
-m = model((5))
-for i in range(10):
-    print(m.act((0)))
 
---
+++ trainer.py
@@ -0,0 +1,39 @@
+import gym
+import model
+
+# Instantiate the Environment
+env = gym.make('SpaceInvaders-v0')
+
+# Instantiate the model
+model = model.Model(env.action_space.shape)
+
+# Gather first observation
+obs = env.reset()
+
+# Training Limits
+MAX_EPISODES = 10
+
+# Training Loop
+episode = 0
+episode_step = 0
+total_step = 0
+episode_reward = 0
+total_reward = 0
+while(episode < MAX_EPISODES):
+    action = model.act(obs)
+    obs, reward, done, info = env.step(action)
+    episode_reward += reward
+    total_reward += reward
+    episode_step += 1
+    total_step += 1
+    #env.render()
+
+    # Episode has finished
+    if(done):
+        print("Episode", str(episode), "finished after", episode_step, "steps with", str(episode_reward), "reward")
+        episode += 1
+        episode_step = 0
+        episode_reward = 0
+        obs = env.reset()
+
+print("Training finished after", str(episode), "episodes and", str(total_step), "steps with avg reward", str(total_reward/episode), "reward/episode")
