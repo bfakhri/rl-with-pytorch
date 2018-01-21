1767427529790e8193b1dfd5f75c451661dcec71
tubi <bfakhri@asu.edu>
2018-01-21 12:56:29 -0700

    Finished replay buffer, working

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-class Model():
+class Model(torch.nn.Module):
     def __init__(self, obs_shape, act_size):
         # Number of possible actions
         self.act_size = act_size
@@ -13,15 +13,15 @@ class Model():
         flat_obs = dummy_obs.reshape(-1)
 
         # Define a single layer for the action
-        self.policy = torch.nn.Linear(len(flat_obs), act_size)
+        nn_out = torch.nn.Linear(len(flat_obs), act_size)
+        self.policy = torch.nn.functional.softmax(nn_out) 
 
 
-    def act(self, obs):
+    def act_probs(self, obs):
         " Right now it is just a random agent"
         obs_torch = torch.autograd.Variable(torch.Tensor.float(torch.from_numpy(obs.reshape(-1))))
-        policy_output = self.policy(obs_torch)
-        np_argmax = np.argmax(policy_output.data.numpy())
-        return np_argmax
+        policy_output = self.policy(obs_torch).data.numpy()
+        return policy_output 
 
     def learn(self, replay_buffer):
         "Performs backprop w.r.t. the replay buffer"
--- trainer.py
+++ trainer.py
@@ -2,25 +2,37 @@ import gym
 import model
 import numpy as np
 
-#class ReplayBuffer():
-    def __init__(self, obs_shape):
+class ReplayBuffer():
+    def __init__(self, obs_shape, act_shape):
         # Number of steps in the buffer
         self.n = 0
         # Observations
-        shape_with_time = (1,) + obs_shape
-        self.obs = np.empty(shape_with_time, dtype=np.float)
+        self.obs = np.empty((1,) + obs_shape, dtype=np.float)
+        # Actions
+        self.acts = np.empty((1,) + act_shape, dtype=np.float)
         # Rewards
-        self.r = np.empty((1,), dtype=np.float)
+        self.rs = np.empty((1,), dtype=np.float)
 
+    def append(self, ob, act, r):
+        if(self.n == 0):
+            self.obs[0] = ob
+            self.acts[0] = act
+            self.rs[0] = r
 
+        else:
+            self.obs = np.append(self.obs, np.expand_dims(ob, 0), axis=0)
+            self.acts = np.append(self.acts, np.expand_dims(act, 0), axis=0)
+            self.rs = np.append(self.rs, r)
 
+        self.n += 1
 
 # Instantiate the Environment
 env = gym.make('SpaceInvaders-v0')
 
 # Instantiate the model
 model = model.Model(env.observation_space.shape, env.action_space.n) 
-
+# Instantiate replay buffer
+rp_buffer = ReplayBuffer(env.observation_space.shape, (env.action_space.n,)) 
 # Gather first observation
 obs = env.reset()
 
@@ -33,14 +45,25 @@ episode_step = 0
 total_step = 0
 episode_reward = 0
 total_reward = 0
+nsteps_to_learn = 40
+
+# Training loop
 while(episode < MAX_EPISODES):
-    action = model.act(obs)
+    act_probs = model.act_probs(obs)
+    action = np_argmax = np.argmax(act_probs)
     obs, reward, done, info = env.step(action)
     episode_reward += reward
     total_reward += reward
     episode_step += 1
     total_step += 1
     env.render()
+    # Add experience to replay buffer
+    rp_buffer.append(obs, act_probs, reward)
+    
+    # Learn from experience and clear rp buffer
+    if(total_step%nsteps_to_learn == 0):
+        model.learn(rp_buffer)
+        rp_buffer = ReplayBuffer(env.observation_space.shape, (env.action_space.n,))  
 
     # Episode has finished
     if(done):
