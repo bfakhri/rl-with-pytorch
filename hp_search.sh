49c685d75f504a5d59b95c7df6226384e86f7268
Bijan Fakhri <bfakhri@asu.edu>
2018-02-12 13:51:47 -0700

    init

---
+++ hp_search.sh
@@ -0,0 +1,13 @@
+#!/bin/sh 
+tmux new-session -s 'train' -d 'watch -n 0.1 nvidia-smi'
+tmux split-window -v 'htop'
+tmux split-window -h 'tensorboard --logdir=./runs/ --port=12345'
+tmux set -g remain-on-exit on
+tmux new-window 'python trainer.py -b=20'
+tmux new-window 'python trainer.py -b=40'
+tmux new-window 'python trainer.py -b=60'
+tmux new-window 'python trainer.py -b=80'
+tmux new-window 'python trainer.py -b=100'
+tmux new-window 'python trainer.py -b=120'
+#tmux select-window -t 0
+tmux -2 attach-session -d 
