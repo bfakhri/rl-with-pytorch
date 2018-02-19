6ec788748f85bdc3c439f8c5edcad9927b83de2a
Kyle Shumway (Student) <ASUAD\ksshumwa@en4146051l.fulton.ad.asu.edu>
2018-02-19 02:24:17 -0700

    tmux shell script fix for hp_search

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
-tmux new -s "monitor" -d "python trainer.py"
-#tmux split-window -h "python time.py" #<---- write this file
+tmux new-session -s "monitor" -d
+tmux send-keys -t "monitor" "./hp_search.sh" C-m
 tmux split-window -v "htop"
 tmux split-window -h "watch -n 0.1 nvidia-smi"
-tmux set-g remain-on-exit on
-tmux new-window "Tensorboard --logdir=./logs/ port=0000"
+tmux set -g remain-on-exit on
+tmux new-window "Tensorboard --logdir=./logs/ port=12345"
+tmux select-window -t 0
 tmux -2 attach-session -d
+#tmux split-window -h "python time.py" #<---- write this file
