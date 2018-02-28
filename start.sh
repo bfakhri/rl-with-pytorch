b66b52c776f00997a9b1409db5921c338883239d
Kyle Shumway (Student) <ASUAD\ksshumwa@en4146051l.fulton.ad.asu.edu>
2018-02-28 13:04:54 -0700

    finished hp_search and updated start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,10 @@ tmux set -g remain-on-exit on
 tmux new-window "Tensorboard --logdir=./logs/ port=12345"
 tmux select-window -t 0
 tmux -2 attach-session -d
+
+
+#TODO
+#utilize $@ array to pass args to ./hpsearch for bsize
+#create parser to search for bNUM (where NUM is an int) in array and set those for bsize
+#create documentation for the rest of the group :D
 #tmux split-window -h "python time.py" #<---- write this file
